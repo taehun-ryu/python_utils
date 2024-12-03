dfb6d9e565269a62c8c8659be6d87d43a6b2e18b
taehun-ryu <xogns2079@gmail.com>
2024-12-03 14:54:07 +0900

    refact: Integrate to CLASS

--- calibration/CalibrationYAML.py
+++ calibration/CalibrationYAML.py
@@ -1,97 +1,108 @@
 import numpy as np
 import yaml
 import cv2
+from functools import singledispatchmethod
 
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import Quaternion as Q
 
+class CalibrationYAML:
+    data = None
 
-def read_yaml(file_path):
-    with open(file_path, 'r') as file:
-        data = yaml.safe_load(file)
-    return data
+    def __init__(self, file_path):
+        with open(file_path, 'r') as file:
+            self.data = yaml.safe_load(file)
 
-def scale_intrinsics(camera_matrix, original_size, new_size):
-    scale_x = new_size[0] / original_size[0]
-    scale_y = new_size[1] / original_size[1]
-    scaled_matrix = camera_matrix.copy()
-    scaled_matrix[0, 0] *= scale_x  # fx
-    scaled_matrix[1, 1] *= scale_y  # fy
-    scaled_matrix[0, 2] *= scale_x  # cx
-    scaled_matrix[1, 2] *= scale_y  # cy
-    return scaled_matrix
+    def scale_intrinsics(self, camera_matrix, original_size, new_size):
+        scale_x = new_size[0] / original_size[0]
+        scale_y = new_size[1] / original_size[1]
+        scaled_matrix = camera_matrix.copy()
+        scaled_matrix[0, 0] *= scale_x  # fx
+        scaled_matrix[1, 1] *= scale_y  # fy
+        scaled_matrix[0, 2] *= scale_x  # cx
+        scaled_matrix[1, 2] *= scale_y  # cy
+        return scaled_matrix
 
-def save_camera_info_yaml(file_path, camera_name, image_width, image_height, camera_matrix, distortion_coeffs, rectification_matrix, projection_matrix):
-    camera_info = {
-        "camera_name": camera_name,
-        "image_width": image_width,
-        "image_height": image_height,
-        "camera_matrix": {
-            "rows": 3,
-            "cols": 3,
-            "data": camera_matrix.flatten().tolist()
-        },
-        "distortion_model": "plumb_bob",
-        "distortion_coefficients": {
-            "rows": 1,
-            "cols": len(distortion_coeffs),
-            "data": distortion_coeffs.flatten().tolist()
-        },
-        "rectification_matrix": {
-            "rows": 3,
-            "cols": 3,
-            "data": rectification_matrix.flatten().tolist()
-        },
-        "projection_matrix": {
-            "rows": 3,
-            "cols": 4,
-            "data": projection_matrix.flatten().tolist()
+    def save_camera_info_yaml(self, file_path, camera_name, image_width, image_height, camera_matrix, distortion_coeffs, rectification_matrix, projection_matrix):
+        camera_info = {
+            "camera_name": camera_name,
+            "image_width": image_width,
+            "image_height": image_height,
+            "camera_matrix": {
+                "rows": 3,
+                "cols": 3,
+                "data": camera_matrix.flatten().tolist()
+            },
+            "distortion_model": "plumb_bob",
+            "distortion_coefficients": {
+                "rows": 1,
+                "cols": len(distortion_coeffs),
+                "data": distortion_coeffs.flatten().tolist()
+            },
+            "rectification_matrix": {
+                "rows": 3,
+                "cols": 3,
+                "data": rectification_matrix.flatten().tolist()
+            },
+            "projection_matrix": {
+                "rows": 3,
+                "cols": 4,
+                "data": projection_matrix.flatten().tolist()
+            }
         }
-    }
 
-    # YAML 저장
-    with open(file_path, 'w') as file:
-        file.write("#%YAML:1.0\n")  # YAML 버전 표기
-        yaml.dump(camera_info, file, default_flow_style=None, sort_keys=False)
+        # YAML 저장
+        with open(file_path, 'w') as file:
+            file.write("#%YAML:1.0\n")  # YAML 버전 표기
+            yaml.dump(camera_info, file, default_flow_style=None, sort_keys=False)
 
-def print_left_to_right_tf(right_pose): # right_pose: 4x4
-    translation = right_pose[:3, 3]
-    rotation = right_pose[:3, :3]
-    q = Q.rotation_matrix_to_quaternion(rotation)
-    print("tf: tx ty tz q1 q2 q3 q0")
-    print(f"{translation[0]}, {translation[1]}, {translation[2]}, {q[1]}, {q[1]}, {q[3]}, {q[0]}")
+    @singledispatchmethod
+    def print_tf(self, arg):
+        raise NotImplementedError("Unsupported type")
 
-def print_left_to_right_tf(R, t):
-    q = Q.rotation_matrix_to_quaternion(R)
-    print("tf: tx ty tz q1 q2 q3 q0")
-    print(f"{t[0]}, {t[1]}, {t[2]}, {q[1]}, {q[1]}, {q[3]}, {q[0]}")
+    @print_tf.register
+    def _(self, right_pose: np.ndarray):
+        if right_pose.shape == (4, 4):
+            translation = right_pose[:3, 3]
+            rotation = right_pose[:3, :3]
+            q = Q.rotation_matrix_to_quaternion(rotation)
+            print("tf: tx ty tz q1 q2 q3 q0")
+            print(f"{translation[0]}, {translation[1]}, {translation[2]}, {q[1]}, {q[2]}, {q[3]}, {q[0]}")
 
-def calculate_rectification_matrices(left_intrinsic, left_distortion, right_intrinsic, right_distortion, 
-                                     rotation, translation, image_size):
-    """
-    Calculate rectification matrices for stereo cameras.
+    @print_tf.register
+    def _(self, args: tuple):
+        if len(args) == 2:
+            R, t = args
+            q = Q.rotation_matrix_to_quaternion(R)
+            print("tf: tx ty tz q1 q2 q3 q0")
+            print(f"{t[0]}, {t[1]}, {t[2]}, {q[1]}, {q[2]}, {q[3]}, {q[0]}")
 
-    Parameters:
-        left_intrinsic (np.array): Intrinsic matrix of the left camera.
-        left_distortion (np.array): Distortion coefficients of the left camera.
-        right_intrinsic (np.array): Intrinsic matrix of the right camera.
-        right_distortion (np.array): Distortion coefficients of the right camera.
-        rotation (np.array): Rotation matrix from left to right camera.
-        translation (np.array): Translation vector from left to right camera.
-        image_size (tuple): Image size as (width, height).
+    def calculate_rectification_matrices(self, left_intrinsic, left_distortion, right_intrinsic, right_distortion, 
+                                        rotation, translation, image_size):
+        """
+        Calculate rectification matrices for stereo cameras.
 
-    Returns:
-        R1, R2: Rectification matrices for the left and right cameras.
-        P1, P2: Projection matrices after rectification for the left and right cameras.
-        Q: Disparity-to-depth mapping matrix.
-    """
-    # Compute rectification transforms
-    R1, R2, P1, P2, Q, _, _ = cv2.stereoRectify(
-        left_intrinsic, left_distortion,
-        right_intrinsic, right_distortion,
-        image_size, rotation, translation,
-        flags=cv2.CALIB_ZERO_DISPARITY, alpha=0
-    )
-    return R1, R2, P1, P2, Q
+        Parameters:
+            left_intrinsic (np.array): Intrinsic matrix of the left camera.
+            left_distortion (np.array): Distortion coefficients of the left camera.
+            right_intrinsic (np.array): Intrinsic matrix of the right camera.
+            right_distortion (np.array): Distortion coefficients of the right camera.
+            rotation (np.array): Rotation matrix from left to right camera.
+            translation (np.array): Translation vector from left to right camera.
+            image_size (tuple): Image size as (width, height).
+
+        Returns:
+            R1, R2: Rectification matrices for the left and right cameras.
+            P1, P2: Projection matrices after rectification for the left and right cameras.
+            Q: Disparity-to-depth mapping matrix.
+        """
+        # Compute rectification transforms
+        R1, R2, P1, P2, Q, _, _ = cv2.stereoRectify(
+            left_intrinsic, left_distortion,
+            right_intrinsic, right_distortion,
+            image_size, rotation, translation,
+            flags=cv2.CALIB_ZERO_DISPARITY, alpha=0
+        )
+        return R1, R2, P1, P2, Q
--- calibration/opencv_calib_to_camera_info.py
+++ calibration/opencv_calib_to_camera_info.py
@@ -3,25 +3,25 @@ from CalibrationYAML import *
 
 def main():
     file_path = 'calib_result.yaml'
-    data = read_yaml(file_path)
+    calib = CalibrationYAML(file_path)
 
     # image size
     size = (640, 480)
 
     # intrinsic parameter
-    left_intrinsic = np.array(data['mtx_left']['data']).reshape(3, 3)
-    right_intrinsic = np.array(data['mtx_right']['data']).reshape(3, 3)
+    left_intrinsic = np.array(calib.data['mtx_left']['data']).reshape(3, 3)
+    right_intrinsic = np.array(calib.data['mtx_right']['data']).reshape(3, 3)
 
     # distortion parameter
-    left_distortion = np.array(data['dist_left']['data'])
-    right_distortion = np.array(data['dist_right']['data'])
+    left_distortion = np.array(calib.data['dist_left']['data'])
+    right_distortion = np.array(calib.data['dist_right']['data'])
 
     # right camera’s pose in the left camera’s frame
-    camera_rotation = np.array(data['R']['data']).reshape(3, 3)
-    camera_translation = np.array(data['T']['data']).reshape(3, 1).flatten()
+    camera_rotation = np.array(calib.data['R']['data']).reshape(3, 3)
+    camera_translation = np.array(calib.data['T']['data']).reshape(3, 1).flatten()
 
     # Calculate rectification matrices
-    R1, R2, P1, P2, Q = calculate_rectification_matrices(
+    R1, R2, P1, P2, Q = calib.calculate_rectification_matrices(
         left_intrinsic, left_distortion,
         right_intrinsic, right_distortion,
         camera_rotation, camera_translation,
@@ -29,10 +29,10 @@ def main():
     )
 
     # ros camera_info에 맞게 yaml 파일로 저장
-    save_camera_info_yaml("left.yaml", "left_camera", size[0], size[1], left_intrinsic, left_distortion, R1, P1)
-    save_camera_info_yaml("right.yaml", "right_camera", size[0], size[1], right_intrinsic, right_distortion, R2, P2)
+    calib.save_camera_info_yaml("left.yaml", "left_camera", size[0], size[1], left_intrinsic, left_distortion, R1, P1)
+    calib.save_camera_info_yaml("right.yaml", "right_camera", size[0], size[1], right_intrinsic, right_distortion, R2, P2)
 
-    print_left_to_right_tf(camera_rotation, camera_translation)
+    calib.print_tf((camera_rotation, camera_translation))
 
 if __name__ == '__main__':
     main()
