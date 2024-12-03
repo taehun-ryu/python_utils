f6a804665c6c9925c5f4dd7ddbdb1bc20661ef9d
taehun-ryu <xogns2079@gmail.com>
2024-12-03 17:19:16 +0900

    feat: generalize for camera pose matrix

--- calibration/mc_calib_to_camera_info.py
+++ calibration/mc_calib_to_camera_info.py
@@ -1,71 +1,65 @@
 import numpy as np
 from CalibrationYAML import *
 
-def determine_cameras(data):
-    cam_0_pose = np.array(data['camera_0']['camera_pose_matrix']['data']).reshape(4, 4)
-    cam_1_pose = np.array(data['camera_1']['camera_pose_matrix']['data']).reshape(4, 4)
+class McCalib:
+    cam_0_pose = None
+    cam_1_pose = None
+    left_camera = None
+    right_camera = None
+    camera_rotation = None
+    camera_translation = None
+    camera_pose_matrix = None
 
-    if cam_1_pose[0, 3] > cam_0_pose[0, 3]:
-        left_camera, right_camera = 'camera_0', 'camera_1'
-        is_based_on_left = True
-    else:
-        left_camera, right_camera = 'camera_1', 'camera_0'
-        is_based_on_left = False
+    def __init__(self, data):
+        self.cam_0_pose = np.array(data['camera_0']['camera_pose_matrix']['data']).reshape(4, 4)
+        self.cam_1_pose = np.array(data['camera_1']['camera_pose_matrix']['data']).reshape(4, 4)
+        self.camera_pose_matrix = self.cam_1_pose
 
-    return left_camera, right_camera, is_based_on_left
+        if self.cam_1_pose[0, 3] > self.cam_0_pose[0, 3]:
+            self.left_camera, self.right_camera = 'camera_0', 'camera_1'
+            self.camera_rotation = self.camera_pose_matrix[:3, :3].T
+            self.camera_translation = np.matmul(-self.camera_rotation, self.camera_pose_matrix[:3, 3]) * 0.01 
+        else:
+            self.left_camera, self.right_camera = 'camera_1', 'camera_0'
+            self.camera_rotation = self.camera_pose_matrix[:3, :3]
+            self.camera_translation = self.camera_pose_matrix[:3, 3] * 0.01 
 
-def set_camera_pose_matrix(camera_pose_matrix, is_based_on_left):
-    if is_based_on_left:
-        # from the coordinate system of the first camera to the second camera
-        camera_rotation = camera_pose_matrix[:3, :3]
-        camera_translation = np.matmul(camera_rotation, camera_pose_matrix[:3, 3] * 0.01)
-    else:
-        raise NotImplementedError("Not implemented yet") #TODO: how can i get the correct R ant t?
-
-    camera_rotation = camera_pose_matrix[:3, :3]
-    camera_translation = camera_pose_matrix[:3, 3]
-
-    return camera_rotation, camera_translation
+        
+            
 
 def main():
     file_path = 'calib_result.yaml'
     calib = CalibrationYAML(file_path)
-
-    # 둘 중 어느 카메라가 왼쪽, 오른쪽인지 결정
-    left_camera, right_camera, is_based_on_left = determine_cameras(calib.data)
+    mccalib = McCalib(calib.data)
 
     # image size
-    original_size = (calib.data[left_camera]['img_width'], calib.data[left_camera]['img_height'])
+    original_size = (calib.data[mccalib.left_camera]['img_width'], calib.data[mccalib.left_camera]['img_height'])
     new_size = (640, 480)
 
     # intrinsic parameter
-    left_intrinsic = np.array(calib.data[left_camera]['camera_matrix']['data']).reshape(3, 3)
-    right_intrinsic = np.array(calib.data[right_camera]['camera_matrix']['data']).reshape(3, 3)
+    left_intrinsic = np.array(calib.data[mccalib.left_camera]['camera_matrix']['data']).reshape(3, 3)
+    right_intrinsic = np.array(calib.data[mccalib.right_camera]['camera_matrix']['data']).reshape(3, 3)
 
     # distortion parameter
-    left_distortion = np.array(calib.data[left_camera]['distortion_vector']['data'])
-    right_distortion = np.array(calib.data[right_camera]['distortion_vector']['data'])
+    left_distortion = np.array(calib.data[mccalib.left_camera]['distortion_vector']['data'])
+    right_distortion = np.array(calib.data[mccalib.right_camera]['distortion_vector']['data'])
 
     # scale intrinsic parameter
     left_intrinsic_scaled = calib.scale_intrinsics(left_intrinsic, original_size, new_size)
     right_intrinsic_scaled = calib.scale_intrinsics(right_intrinsic, original_size, new_size)
 
-    # right camera’s pose in the left camera’s frame
-    camera_pose_matrix = np.array(calib.data[right_camera]['camera_pose_matrix']['data']).reshape(4, 4)
-    R, t = set_camera_pose_matrix(camera_pose_matrix, is_based_on_left)
-
     # Calculate rectification matrices
     R1, R2, P1, P2, Q = calib.calculate_rectification_matrices(
         left_intrinsic_scaled, left_distortion,
         right_intrinsic_scaled, right_distortion,
-        R, t, new_size
+        mccalib.camera_rotation, mccalib.camera_translation, new_size
     )
 
     # ros camera_info에 맞게 yaml 파일로 저장
     calib.save_camera_info_yaml("left.yaml", "left_camera", new_size[0], new_size[1], left_intrinsic_scaled, left_distortion, R1, P1)
     calib.save_camera_info_yaml("right.yaml", "right_camera", new_size[0], new_size[1], right_intrinsic_scaled, right_distortion, R2, P2)
 
-    calib.print_tf(camera_pose_matrix)
+    calib.print_tf(mccalib.camera_pose_matrix)
 
 if __name__ == '__main__':
     main()
--- calibration/opencv_calib_to_camera_info.py
+++ calibration/opencv_calib_to_camera_info.py
@@ -9,16 +9,16 @@ def main():
     size = (640, 480)
 
     # intrinsic parameter
-    left_intrinsic = np.array(calib.data['mtx_left']['data']).reshape(3, 3)
-    right_intrinsic = np.array(calib.data['mtx_right']['data']).reshape(3, 3)
+    left_intrinsic = np.array(calib.data['Camera_Matrix_1']['data']).reshape(3, 3)
+    right_intrinsic = np.array(calib.data['Camera_Matrix_2']['data']).reshape(3, 3)
 
     # distortion parameter
-    left_distortion = np.array(calib.data['dist_left']['data'])
-    right_distortion = np.array(calib.data['dist_right']['data'])
+    left_distortion = np.array(calib.data['Distortion_Coefficients_1']['data'])
+    right_distortion = np.array(calib.data['Distortion_Coefficients_2']['data'])
 
     # right camera’s pose in the left camera’s frame
-    camera_rotation = np.array(calib.data['R']['data']).reshape(3, 3)
-    camera_translation = np.array(calib.data['T']['data']).reshape(3, 1).flatten()
+    camera_rotation = np.array(calib.data['Rotation_Matrix']['data']).reshape(3, 3)
+    camera_translation = np.array(calib.data['Translation_Vector']['data']).reshape(3, 1).flatten()
 
     # Calculate rectification matrices
     R1, R2, P1, P2, Q = calib.calculate_rectification_matrices(
@@ -32,7 +32,7 @@ def main():
     calib.save_camera_info_yaml("left.yaml", "left_camera", size[0], size[1], left_intrinsic, left_distortion, R1, P1)
     calib.save_camera_info_yaml("right.yaml", "right_camera", size[0], size[1], right_intrinsic, right_distortion, R2, P2)
 
-    calib.print_tf((camera_rotation, camera_translation))
+    calib.print_tf((camera_rotation.T, -camera_rotation.T @ camera_translation))
 
 if __name__ == '__main__':
     main()
